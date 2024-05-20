eccce884dcf677ecaabe9efeb59fc776d68e821c
srejus <srejusjayakumar@gmail.com>
2024-05-20 19:10:27 +0530

    update and delete restaurant added

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,6 +2,11 @@ const Restaurant = require("../models/restaurantModel");
 
 const getRestaurants = async(req,res) => {
     try{
+        const {id} = req.params;
+        if(id) {
+            const restaurant = await Restaurant.findById(id);
+            return res.status(200).json(restaurant);
+        }
         const restaurants = await Restaurant.find();
         res.status(200).json(restaurants);
     }catch(err) {
@@ -29,9 +34,20 @@ const updateRestaurant = async(req,res) => {
         if(!restaurant) {
             return res.status(404).json({error:"No restaurant found with the given id!"});
         }
-        const updatedData = await Restaurant.findByIdAndUpdate(id,req.data);
-        res.status(200).json(updatedData);
+        const updatedData = await Restaurant.findByIdAndUpdate(id,req.body);
+        const updatedRestaurant = await Restaurant.findById(id);
+        res.status(200).json(updatedRestaurant);
     }catch(err) {
+        res.status(400).json({error:err.message});
+    }
+}
+
+const deleteRestaurant = async(req,res) => {
+    try{
+        const {id} = req.params;
+        const restaurant = await Restaurant.findByIdAndDelete(id);
+        res.status(200).json({message:"Restaurant deleted successfully!"});
+    } catch(err) {
         res.status(400).json({error:err.message});
     }
 }
@@ -40,5 +56,6 @@ const updateRestaurant = async(req,res) => {
 module.exports = {
     getRestaurants,
     createRestaurant,
-    updateRestaurant
+    updateRestaurant,
+    deleteRestaurant
 }
--- routes/restaurantRoutes.js
+++ routes/restaurantRoutes.js
@@ -2,15 +2,17 @@ const express = require('express');
 
 const auth = require("../middlewares/authMiddleware");
 
-const {getRestaurants,createRestaurant,updateRestaurant} = require('../controllers/restaurantController');
+const {getRestaurants,createRestaurant,updateRestaurant,deleteRestaurant} = require('../controllers/restaurantController');
 
 const router = express.Router();
 
 // routes
 
 router.get("/",getRestaurants);
+router.get("/:id",getRestaurants),
 router.post("/create",auth, createRestaurant);
 router.put("/edit/:id",auth, updateRestaurant);
+router.delete("/delete/:id",auth, deleteRestaurant);
 
 
 module.exports = router;
